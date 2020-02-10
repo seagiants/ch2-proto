92dde0b2d05d5080bd1772170292a95bbe67ef3e
romaintailhurat <romain.tailhurat@gmail.com>
2020-02-11 00:09:13 +0100

    Connexions buttons

---
+++ ClientButton.gd
@@ -0,0 +1,11 @@
+extends Button
+
+var SERVER_IP = "127.0.0.1"
+var SERVER_PORT = 9000
+
+func _pressed():
+	print("Connection as a client to: ", SERVER_IP, ":", SERVER_PORT)
+	var peer = NetworkedMultiplayerENet.new()
+	peer.create_client(SERVER_IP, SERVER_PORT)
+	get_tree().set_network_peer(peer)
+	print(get_tree().is_network_server())
