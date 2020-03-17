15f7a524c29c2ae5cdc1c132d0dfb580355cc5e9
Eric Sigaud <bibirico@yahoo.fr>
2020-03-17 09:03:57 +0100

    Fixing loco_stats

--- scenes/PlayerBoard/LocoStats/LocoStats.gd
+++ scenes/PlayerBoard/LocoStats/LocoStats.gd
@@ -7,12 +7,12 @@ var stat_box
 func _ready():
 	stat_box = get_node("HSplitContainer/LocoStats"	)
 	GameState.players[0].connect("loco_stats_changed",self,"on_loco_stats_changed")
-	on_loco_stats_changed()
+	on_loco_stats_changed(0)
 
-func on_loco_stats_changed():
+func on_loco_stats_changed(player_index):
 	for i in range(stat_box.get_child_count()):
-		get_child(i).queue_free()
-	var nstats = GameState.players[0].get_loco_stats() 
+		stat_box.get_child(i).queue_free()
+	var nstats = GameState.players[player_index].get_loco_stats() 
 	print(nstats)
 	for stat in nstats.keys():
 		var label = StatLabel.instance()
