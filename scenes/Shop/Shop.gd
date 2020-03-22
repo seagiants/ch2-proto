a70204f05f04dc958c1c48f60eb4a025dc611638
Eric Sigaud <bibirico@yahoo.fr>
2020-03-22 15:40:22 +0100

    A bit of tweaking

--- scenes/Shop/Shop.gd
+++ scenes/Shop/Shop.gd
@@ -1,5 +1,6 @@
 extends VBoxContainer
 
+var _dumb_counter = 0
 func _on_Simone_pressed():
 	var selected_path = $VSplitContainer/PathShop.get_path_selected()
 	if selected_path != null :
@@ -7,4 +8,9 @@ func _on_Simone_pressed():
 		GameState.players[1].set_path([0,0,0])
 		var _changed = get_tree().change_scene("res://scenes/Quest/Quest.tscn")
 	else : 
-		print("Faut choisir un path !")
+		_dumb_counter += 1
+		if _dumb_counter < 5 : 
+			$Button.set_text("En voiture Simone ! (ouais enfin quand on saura où on va...)")
+			print("Faut choisir un path !")
+		else :
+			$Button.set_text("T'es con ou quoi ?! Choisis un putain de chemin d'abord !!!")
